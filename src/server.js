cd7467041daa03351880243cfd08205c8153c24c
Igor Ferreira <48239402+imagno@users.noreply.github.com>
2022-02-14 09:57:30 -0300

    mapeando rotas sem auxílio de framework

--- src/server.js
+++ src/server.js
@@ -3,9 +3,21 @@ const http = require('http');
 http
   .createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'Application/json' });
-    
-    res.end(JSON.stringify({
-      message: 'Hello Node!'
-    }));
+
+    if(req.url === '/produto') {
+      res.end(
+        JSON.stringify({
+          message: 'Rota de produto'
+        })
+      );
+    }
+
+    if(req.url === '/usuario') {
+      res.end(
+        JSON.stringify({
+          message: 'Rota de usuário'
+        })
+      );
+    }
   })
   .listen(4001, () => console.log('Servidor rodando na porta 4001'));
